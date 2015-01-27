9e250ce046d68ce6e14b8d9dfd950f33e5a87587
sj cho <juns0201@gmail.com>
2015-01-27 10:36:05 +0900

    Tab jacking

--- App570/app/src/main/java/com/example/juns/app570/MainActivity.java
+++ App570/app/src/main/java/com/example/juns/app570/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.juns.app570;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +16,25 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mOnClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnad:
+                Toast.makeText(this, "광고를 보았습니다.", 1).show();
+                break;
+            case R.id.btncard:
+                Toast.makeText(this, "물품 대금을 카드 결제합니다.", 1).show();
+                break;
+            case R.id.calltoast:
+                LinearLayout linear = (LinearLayout) View.inflate(this, R.layout.faketoast, null);
+                Toast fake = new Toast(this);
+                fake.setView(linear);
+                fake.setGravity(Gravity.LEFT | Gravity.TOP, 0, 20);
+                fake.setDuration(Toast.LENGTH_LONG);
+                fake.show();
+                break;
+        }
     }
 
 
