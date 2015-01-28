2c1ca395d3615444111cb1fc142a7c42f11496df
sj cho <juns0201@gmail.com>
2015-01-28 18:25:28 +0900

    ExpandableListAdapter

--- ExpandableListView/app/src/main/java/com/example/juns/expandablelistview/MainActivity.java
+++ ExpandableListView/app/src/main/java/com/example/juns/expandablelistview/MainActivity.java
@@ -4,14 +4,59 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.SimpleExpandableListAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class MainActivity extends ActionBarActivity {
+    ExpandableListView mList;
+    String[] arProv = new String[] {"충청도", "경기도", "강원도"};
+    String[][] arCity = new String[][] {
+            {"논산", "당진", "부여"},
+            {"수원", "용인"},
+            {"춘천", "원주", "홍천", "강릉"}
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mList = (ExpandableListView) findViewById(R.id.list);
+        List<Map<String, String>> provData = new ArrayList<Map<String, String>>();
+        List<List<Map<String, String>>> cityData = new ArrayList<List<Map<String, String>>>();
+        for (int i=0; i<arProv.length; i++) {
+            Map<String, String> Prov = new HashMap<String, String>();
+            Prov.put("prov", arProv[i]);
+            provData.add(Prov);
+
+            List<Map<String, String>> children = new ArrayList<Map<String, String>>();
+            for (int j=0; j<arCity[i].length; j++) {
+                Map<String, String> City = new HashMap<String, String>();
+                City.put("city", arCity[i][j]);
+                children.add(City);
+            }
+            cityData.add(children);
+        }
+
+        ExpandableListAdapter adapter = new SimpleExpandableListAdapter(
+                this,
+                provData,
+                android.R.layout.simple_expandable_list_item_1,
+                new String[] {"prov"},
+                new int[] { android.R.id.text1},
+                cityData,
+                android.R.layout.simple_expandable_list_item_1,
+                new String[] {"city"},
+                new int[] {android.R.id.text1}
+        );
+        mList.setAdapter(adapter);
     }
 
 
