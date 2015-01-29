bd10b300ef94d776f1bf5b62073b414bd569cc3f
sj cho <juns0201@gmail.com>
2015-01-29 15:53:42 +0900

    GridView

--- GridView/app/src/main/java/com/example/juns/gridview/MainActivity.java
+++ GridView/app/src/main/java/com/example/juns/gridview/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.juns.gridview;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +20,17 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView grid = (GridView) findViewById(R.id.grid);
+        ImageAdapter adapter = new ImageAdapter(this);
+
+        grid.setAdapter(adapter);
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, position +" 번째 그림 선택", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
@@ -35,5 +54,52 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+}
+
+class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    int[] picture = {
+        R.drawable.ccdam,
+        R.drawable.soyang2,
+        R.drawable.ududong,
+        R.drawable.zipdarigol,
+        R.drawable.dongul
+    };
+
+    ImageAdapter(Context c) {
+        this.mContext = c;
+    }
+
+    @Override
+    public int getCount() {
+        return 50;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return picture[position % 5];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(80, 60));
+            imageView.setAdjustViewBounds(false);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        }else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(picture[position % 5]);
+        return imageView;
     }
 }
